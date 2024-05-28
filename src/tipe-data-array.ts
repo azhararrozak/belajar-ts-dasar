bb9346a34a5ce1267e29e91ca8455e46c20e97cf
azhararrozak <azhardrozak2001@upi.edu>
2024-05-28 13:19:20 +0700

    Belajar Tipe Data

--- src/tipe-data-array.ts
+++ src/tipe-data-array.ts
@@ -1,2 +1,7 @@
 const kendaraan: string[] = ['mobil', 'motor', 'pesawat'];
 console.log(kendaraan); // Output: [ 'mobil', 'motor', 'pesawat' ]
+
+const Perusahaan: Readonly<string[]> = ['Google', 'Facebook', 'Microsoft']; 
+//Perusahaan[0] = 'Apple'; // Error: Cannot assign to '0' because it is a read-only property.
+//Perusahaan.push('Amazon'); // Error: Property 'push' does not exist on type 'readonly string[]'.
+console.log(Perusahaan); // Output: [ 'Google', 'Facebook', 'Microsoft' ]
